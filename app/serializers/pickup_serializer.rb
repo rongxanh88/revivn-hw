0ac4b13bb2759e33686779755b20db554bf63701
Bao Nguyen <rongxanh88@gmail.com>
2023-08-20 13:44:27 -0600

    Add additional attributes to pickup serializer

--- app/serializers/pickup_serializer.rb
+++ app/serializers/pickup_serializer.rb
@@ -1,5 +1,10 @@
 class PickupSerializer < Blueprinter::Base
   identifier :id
 
+  field :address
+  field :address_2, name: :secondary_address
+  field :city
+  field :state
+  field :zip_code
   field :status
 end
--- spec/serializers/pickup_serializer_spec.rb
+++ spec/serializers/pickup_serializer_spec.rb
@@ -9,6 +9,11 @@ describe PickupSerializer do
 
   it 'serializes object to json' do
     expect(serialized[:id]).to eql(pickup.id)
+    expect(serialized[:address]).to eql(pickup.address)
+    expect(serialized[:secondary_address]).to eql(pickup.address_2)
+    expect(serialized[:city]).to eql(pickup.city)
+    expect(serialized[:state]).to eql(pickup.state)
+    expect(serialized[:zip_code]).to eql(pickup.zip_code)
     expect(serialized[:status]).to eql(pickup.status)
   end
 end
